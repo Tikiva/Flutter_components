9d3ebe66722d3ee24b2dbb5d79d9a68b09392f94
LGranados <maui@MacBook-Air-de-LGranados.local>
2022-08-16 00:57:56 -0500

    FLUTTER-Components 0005 rutas de navegacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 //import 'package:fl_components/screens/listview1_screen.dart';
-import 'package:fl_components/screens/listview2_screen.dart';
+
+import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,11 +10,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Listview2Screen(),
+      //home: Listview2Screen(),
       //home: Listview1Screen(),
+      initialRoute: 'home',
+      routes: {
+        'listview1':(BuildContext context ) => const Listview1Screen(),
+        'listview2':(BuildContext context ) => const Listview2Screen(),
+        'alert':(BuildContext context ) => const AlertScreen(),
+        'card':(BuildContext context ) => const CardScreen(),
+        'home':(BuildContext context ) => const HomeScreen(),
+      },
     );
   }
 } 
